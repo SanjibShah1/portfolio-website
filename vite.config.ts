a37cf2bebf5db7062fbddb705a1afb5b3fc89086
Sanjib Shah <lightenworld98@gmail.com>
2025-01-21 21:15:53 +0545

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Use root since a custom domain doesn't have a subpath
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
